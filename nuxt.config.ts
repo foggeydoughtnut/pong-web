1fdd303ece33d489e3902d5724e6bf8d0d90ffe5
Jeff Anderson <foggeydoughtnut@example.com>
2024-09-14 10:57:43 -0600

    added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt'],
-  ssr: false
+  ssr: false,
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
